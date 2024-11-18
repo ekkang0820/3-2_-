5e38d20b8a2c86423fa9513c62c16a6157113cf8
ekkang0820 <104552382+ekkang0820@users.noreply.github.com>
2024-11-18 17:29:24 +0900

    개인정보 수정

--- portfolio/src/components/Skill.jsx
+++ portfolio/src/components/Skill.jsx
@@ -1,39 +1,39 @@
 import React from "react";
 
 const skillText = [
-    {
-      title: "Python(4/5)",
-      desc: "빅데이터 분석, 머신러닝/딥러닝 학습, 데이터 사이언스, 코딩테스트 등을 공부하여 높은 수준의 성취도를 가지고 있습니다.",
-    },
-    {
-      title: "Java(3.5/5)",
-      desc: "학과 커리큘럼에 많이 접하게 되는 언어로, 스프링, 스윙, 안드로이드 등을 하면서 높은 수준의 성취도를 가지고 있습니다.",
-    },
-    {
-      title: "GitHub(2.5/5)",
-      desc: " 프로젝트를 진행하며 지속적으로 사용한 기술로, 준수한 수준의 성취도를 가지고 있습니다.",
-    },
+  {
+    title: "Python",
+    desc: "빅데이터 분석, 시계열 데이터분석",
+  },
+  {
+    title: "LLM",
+    desc: "파인튜닝, 프롬프트 엔지니어링, RAG",
+  },
+  {
+    title: "RPA",
+    desc: " STT데이터를 자동으로 요약 및 필요한 합성데이터를 생성하는 프로젝트",
+  },
 ];
 
 const Skill = () => {
-    return (
-        <section id="skill">
-            <div className="skill__inner">
-                <h2 className="skill__title">
-                    Skills <em>기술 스택</em>
-                </h2>
-                <div className="skill__desc">
-                    {skillText.map((skill, key) => (
-                        <div key={key}>
-                            <span>{key + 1}.</span>
-                            <h3>{skill.title}</h3>
-                            <p>{skill.desc}</p>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <section id="skill">
+      <div className="skill__inner">
+        <h2 className="skill__title">
+          Skills <em>기술 스택</em>
+        </h2>
+        <div className="skill__desc">
+          {skillText.map((skill, key) => (
+            <div key={key}>
+              <span>{key + 1}.</span>
+              <h3>{skill.title}</h3>
+              <p>{skill.desc}</p>
             </div>
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skill;
